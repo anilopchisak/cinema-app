ed762659440127de071103e6f330687cca30b475
Касич Полина Олеговна <alice.sova@mail.ru>
2026-03-10 14:07:12 +0500

    refactor: рефактор cinema page

--- app/cinema/page.tsx
+++ app/cinema/page.tsx
@@ -1,19 +1,29 @@
-import CinemaPage from '@/_pages/CinemaPage/CinemaPage';
-// import { prefetchCinema } from '@/entities/cinema/api/hooks/prefetch-cinema';
-// import { getCinemaParams } from '@/entities/cinema/lib/getCinemaParams';
-// import CinemaContent from '@/widgets/cinema/CinemaContent/CinemaContent';
-// import CinemaIntro from '@/widgets/cinema/CinemaIntro';
-// import { HydrationBoundary } from '@tanstack/react-query';
-import { Suspense } from 'react';
+import { prefetchCinema } from '@/entities/cinema/api/hooks/prefetch-cinema';
+import { getCinemaParams } from '@/entities/cinema/lib/getCinemaParams';
+import CinemaContent from '@/widgets/cinema/CinemaContent/CinemaContent';
+import CinemaIntro from '@/widgets/cinema/CinemaIntro';
+import { HydrationBoundary } from '@tanstack/react-query';
 
 type Props = {
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
 export default async function Cinema({ searchParams }: Props) {
+  const resolvedSearchParams = await searchParams;
+
+  const params = getCinemaParams(resolvedSearchParams);
+
+  const dehydratedState = await prefetchCinema({
+    ...params.apiParams,
+  });
+
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <CinemaPage searchParams={searchParams} />
-    </Suspense>
+    <div>
+      <CinemaIntro />
+
+      <HydrationBoundary state={dehydratedState}>
+        <CinemaContent initialParams={params} />
+      </HydrationBoundary>
+    </div>
   );
 }
