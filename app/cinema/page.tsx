c3bdf255f56e8a6c2ad2877e6dd3d79084c8556c
anilopchisak <poli.key@yandex.ru>
2026-03-11 16:38:40 +0500

    feat: параллельный prefetch в cinema/page

--- app/cinema/page.tsx
+++ app/cinema/page.tsx
@@ -1,8 +1,10 @@
 import { prefetchCinema } from '@/entities/cinema/api/queries/prefetch-cinema';
+import { prefetchFavorites } from '@/entities/favorites/api/queries/prefetch-favorites';
 import { getCinemaParams } from '@/entities/cinema/lib/getCinemaParams';
 import CinemaContent from '@/widgets/cinema/CinemaContent/CinemaContent';
 import CinemaIntro from '@/widgets/cinema/CinemaIntro';
 import { HydrationBoundary } from '@tanstack/react-query';
+import { cookies } from 'next/headers';
 
 type Props = {
   searchParams: { [key: string]: string | string[] | undefined };
@@ -13,7 +15,25 @@ export default async function Cinema({ searchParams }: Props) {
 
   const params = getCinemaParams(resolvedSearchParams);
 
-  const dehydratedState = await prefetchCinema(params.apiParams, params.rawParams.page);
+  const cookieStore = await cookies();
+  const token = cookieStore.get('auth_token')?.value;
+
+  let dehydratedState;
+
+  if (token) {
+    const [cinemaState, favoritesState] = await Promise.all([
+      prefetchCinema(params.apiParams, params.rawParams.page),
+      prefetchFavorites(),
+    ]);
+
+    dehydratedState = {
+      ...cinemaState,
+      queries: [...(cinemaState.queries ?? []), ...(favoritesState.queries ?? [])],
+      mutations: [...(cinemaState.mutations ?? []), ...(favoritesState.mutations ?? [])],
+    };
+  } else {
+    dehydratedState = await prefetchCinema(params.apiParams, params.rawParams.page);
+  }
 
   return (
     <div>
