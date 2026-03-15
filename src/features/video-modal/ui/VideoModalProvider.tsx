649073f4b35aaae6020743feb8d9f0171314d25e
anilopchisak <poli.key@yandex.ru>
2026-03-15 20:06:42 +0500

    fix: правки video modal

--- src/features/video-modal/ui/VideoModalProvider.tsx
+++ src/features/video-modal/ui/VideoModalProvider.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import { videoModalStore } from '../model/video-modal.store';
 import VideoModal from '@/shared/ui/VideoModal';
@@ -7,6 +8,19 @@ import { routes } from '@/shared/config/routes';
 
 const VideoModalProvider = observer(() => {
   const { isOpen, videoUrl, close, filmTitle, filmId } = videoModalStore;
+
+  useEffect(() => {
+    if (!isOpen) {
+      return;
+    }
+
+    const { overflow } = document.body.style;
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      document.body.style.overflow = overflow;
+    };
+  }, [isOpen]);
 
   const openDetails = () => {
     close();
